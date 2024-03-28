36f069b0327d04756add1a97adc0f7841c7d0ca3
Sandunika Dissanayake <sdisanayake238@gmail.com>
2024-03-28 15:42:31 +0530

    Update UI

--- Deal_Management_System/src/app/app.component.ts
+++ Deal_Management_System/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Deal_Management_System_UI';
+  isHomePage: boolean = false;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.isHomePage = (event.url === '/');
+      }
+    });
+  }
 }
